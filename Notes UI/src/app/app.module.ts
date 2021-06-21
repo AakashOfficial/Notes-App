351a517913bb15237e10aa1259487a22aa790c2c
AakashOfficial <aakashtyagi1996@outlook.com>
2021-06-21 09:02:22 +0530

    configuration files

--- Notes UI/src/app/app.module.ts
+++ Notes UI/src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ViewNoteComponent } from './view-note/view-note.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
+import { FooterComponent } from './footer/footer.component';
+import { EditNoteComponent } from './edit-note/edit-note.component';
+import { AddNoteComponent } from './add-note/add-note.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ViewNoteComponent,
+    NavbarComponent,
+    HomeComponent,
+    ForgetPasswordComponent,
+    FooterComponent,
+    EditNoteComponent,
+    AddNoteComponent,
+    AboutusComponent
   ],
   imports: [
     BrowserModule,
