115b9baecafab73a652548cf950dfacb4e2541fc
AakashOfficial <aakashtyagi1996@outlook.com>
2021-06-21 08:57:31 +0530

    view-note component

---
+++ Notes UI/src/app/view-note/view-note.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-note',
+  templateUrl: './view-note.component.html',
+  styleUrls: ['./view-note.component.css']
+})
+export class ViewNoteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
